f75cc30551451887af0a228d76ba1a3e8b656392
abdeldjabar <ahmeidi.abdeldjabar@gmail.com>
2023-03-06 23:09:01 +0100

    add:relation manager

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
+use App\Filament\Resources\PostResource\RelationManagers\TagsRelationManager;
 use App\Models\Post;
 use Closure;
 use Filament\Forms;
@@ -75,7 +76,7 @@ class PostResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            TagsRelationManager::class
         ];
     }
 
